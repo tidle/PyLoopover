fd413009241c39380dd81e2692587d6ef283d7aa
tidle <tidle@users.noreply.github.com>
2019-02-08 16:04:38 -0800

    updated version

--- version.py
+++ version.py
@@ -5,4 +5,4 @@
 # 1.1 : version 1, subversion 2 (1.0 was the first sub-version)
 # 1.1.1 : no difference to the user from version 1.1, but contains bugfixes, or archatecture changes
 # 2.3+ : development version after 2.3. Could become 2.4 or 3.0
-version = "1.2.1"
+version = "1.2.2"
